a3577d290e49f125e29a3e632f614d680feea323
rex-v <frankelandrywork@gmail.com>
2023-11-07 13:14:17 -0600

    Added keys, changed level around a bit

---
+++ Assets/scripts/EnemyDeathZone.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnemyDeathZone : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Orpheus")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+        }
+    }
+}
