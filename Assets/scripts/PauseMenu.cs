9e77a6ed7ad8a76c0facbfd7339fd98ddf545be1
Druasoul <wwwilliam.le@gmail.com>
2023-11-06 21:16:37 -0600

    Added Volume Option
    Added volume Option

--- Assets/scripts/PauseMenu.cs
+++ Assets/scripts/PauseMenu.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 public class PauseMenu : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField] private AudioMixer mainVolume;
+    [SerializeField] private Slider VolumeSlider;
     public static bool GameIsPaused = false;
     public GameObject pauseMenuUI;
-
+    private void Start(){
+        SetVolume();
+    }
     void Update() {
         if(Input.GetKeyDown(KeyCode.Escape)){
             if(GameIsPaused){
@@ -38,5 +44,9 @@ public class PauseMenu : MonoBehaviour
     }
     public void QuitGame(){
         SceneManager.LoadScene("Menuscreen");
+    }
+    public void SetVolume(){
+        float volume = VolumeSlider.value;
+        mainVolume.SetFloat("SFX",Mathf.Log10(volume)*20);
     }
 }
