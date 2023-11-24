9b489cc9c116016054b9353f66e284b734e373e1
Chad Weatherly <chadweatherly2@gmail.com>
2023-11-24 10:48:05 -0600

    Revert "Revert "Merge branch 'master' into Level3GameBalance""
    This reverts commit c7b9df0c3d5ec3b0438a426345380b11257d1656.

--- Assets/scripts/PauseMenu.cs
+++ Assets/scripts/PauseMenu.cs
@@ -7,12 +7,22 @@ using UnityEngine.SceneManagement;
 public class PauseMenu : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField] private AudioMixer mainVolume;
-    [SerializeField] private Slider VolumeSlider;
+    [SerializeField] private AudioMixer SFXMixer;
+    [SerializeField] private AudioMixer MusicMixer;
+    [SerializeField] private Slider SFXSlider;
+    [SerializeField] private Slider MusicSlider;
     public static bool GameIsPaused = false;
     public GameObject pauseMenuUI;
+    public GameObject optionMenuUI;
     private void Start(){
-        SetVolume();
+        if(PlayerPrefs.HasKey("SFXVolume")){
+            LoadVolume();
+            LoadMusicVolume();
+
+        }else{
+            SetVolume();
+            SetMusicVolume();
+        }
     }
     void Update() {
         if(Input.GetKeyDown(KeyCode.Escape)){
@@ -21,7 +31,6 @@ public class PauseMenu : MonoBehaviour
                 Resume();
             } else
             {
-                
                 Pause();
             }
         }
@@ -29,6 +38,7 @@ public class PauseMenu : MonoBehaviour
     public void Resume()
     {
         pauseMenuUI.SetActive(false);
+        optionMenuUI.SetActive(false);
         Time.timeScale = 1f;
         GameIsPaused = false;
         Debug.Log("Game is resumed");
@@ -43,10 +53,32 @@ public class PauseMenu : MonoBehaviour
         AudioListener.pause = true;
     }
     public void QuitGame(){
+        PlayerPrefs.SetString("lastScene", SceneManager.GetActiveScene().name);
+        PlayerPrefs.Save();
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        Debug.Log("Game is resumed");
+        AudioListener.pause = false;
         SceneManager.LoadScene("Menuscreen");
+
     }
     public void SetVolume(){
-        float volume = VolumeSlider.value;
-        mainVolume.SetFloat("SFX",Mathf.Log10(volume)*20);
+        float sfx = SFXSlider.value;
+        SFXMixer.SetFloat("SFX",Mathf.Log10(sfx)*20);
+        PlayerPrefs.SetFloat("SFXVolume", sfx);
     }
+    public void LoadVolume(){
+        SFXSlider.value = PlayerPrefs.GetFloat("SFXVolume");
+        SetVolume();
+    }
+    public void SetMusicVolume(){
+        float music = MusicSlider.value;
+        MusicMixer.SetFloat("Music",Mathf.Log10(music)*20);
+        PlayerPrefs.SetFloat("MusicVolume", music);
+    }
+    public void LoadMusicVolume(){
+        MusicSlider.value = PlayerPrefs.GetFloat("MusicVolume");
+        SetMusicVolume();
+    }
+
 }
